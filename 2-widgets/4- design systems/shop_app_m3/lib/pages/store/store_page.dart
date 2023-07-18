b65b835e21fee5299b924c80ccf4640519c95da9
mohammadhosein13637 <mohammadhosein.rezazadeh@outlook.com>
2023-07-18 11:31:19 +0330

    imgs background removed

--- 2-widgets/4- design systems/shop_app_m3/lib/pages/store/store_page.dart
+++ 2-widgets/4- design systems/shop_app_m3/lib/pages/store/store_page.dart
@@ -13,78 +13,76 @@ class StorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(height: 25),
-          TextField(
-            enabled: false,
-            decoration: InputDecoration(
-              filled: true,
-              hintText: 'search in store',
-              prefixIcon: Padding(
-                padding: const EdgeInsetsDirectional.only(start: 12),
-                child: Icon(Icons.search, color: Colors.black),
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
+    return Column(
+      children: [
+        SizedBox(height: 25),
+        TextField(
+          enabled: false,
+          decoration: InputDecoration(
+            filled: true,
+            hintText: 'search in store',
+            prefixIcon: Padding(
+              padding: const EdgeInsetsDirectional.only(start: 12),
+              child: Icon(Icons.search, color: Colors.black),
             ),
-          ),
-          SizedBox(height: 50),
-          Container(
-            width: double.infinity,
-            height: 120,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
+            border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(15),
-              gradient: LinearGradient(
-                colors: [
-                  Colors.red.shade200,
-                  Colors.yellow.shade400,
-                  Colors.blue.shade600,
-                ],
-              ),
             ),
-            child: Text('Promotions Slider'),
           ),
-          SizedBox(height: 50),
-          Row(
-            children: [
-              Text(
-                'Exclusive Offers',
-                //todo: make styles
-                style: TextStyle(),
-              ),
-              Spacer(),
+        ),
+        SizedBox(height: 50),
+        Container(
+          width: double.infinity,
+          height: 120,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: [
+                Colors.red.shade200,
+                Colors.yellow.shade400,
+                Colors.blue.shade600,
+              ],
+            ),
+          ),
+          child: Text('Promotions Slider'),
+        ),
+        SizedBox(height: 50),
+        Row(
+          children: [
+            Text(
+              'Exclusive Offers',
               //todo: make styles
-              Text(
-                'all',
-                style: TextStyle(),
-              ),
-            ],
-          ),
-          SizedBox(height: 20),
-          SizedBox(
-            height: 350,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              physics: BouncingScrollPhysics(),
-              children: products
-                  .expand(
-                    (element) => [
-                      ProductCard(
-                        product: element,
-                      ),
-                      SizedBox(width: 8),
-                    ],
-                  )
-                  .toList(),
+              style: TextStyle(),
             ),
+            Spacer(),
+            //todo: make styles
+            Text(
+              'all',
+              style: TextStyle(),
+            ),
+          ],
+        ),
+        SizedBox(height: 20),
+        SizedBox(
+          height: 350,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            children: products
+                .expand(
+                  (element) => [
+                    ProductCard(
+                      product: element,
+                    ),
+                    SizedBox(width: 8),
+                  ],
+                )
+                .toList(),
           ),
-          Spacer(),
-        ],
-      ),
+        ),
+        Spacer(),
+      ],
     );
   }
 }
