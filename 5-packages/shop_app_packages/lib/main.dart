0e33490074d6819e63226b4aea935ef3e0467a23
Ali-Fadaei <Ali.Fadaei.424@gmail.com>
2023-10-02 14:02:03 +0330

    package info plus + device info plus

--- 5-packages/shop_app_packages/lib/main.dart
+++ 5-packages/shop_app_packages/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app_packages/modules/app/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const App());
+  // final deviceInfoPlugin = DeviceInfoPlugin();
+  // var winInfo = await deviceInfoPlugin.webBrowserInfo;
 }
